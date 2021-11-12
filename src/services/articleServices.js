dacbf45dda504179ad355c9719211bc955f970b8
minasoha <minasoha@yahoo.com>
2021-11-12 17:32:05 -0500

    finished articleServices

--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,9 +1,16 @@
+import axiosWithAuth from "./../utils/axiosWithAuth";
 
-
-const articleService = ()=> {
-}
+const articleService = async () => {
+  try {
+    const response = await axiosWithAuth().get(
+      "http://localhost:5000/api/articles"
+    );
+    return response;
+  } catch (error) {
+    console.error("COULD NOT FETCH ARTICLES!", error);
+  }
+};
 
 export default articleService;
-
 //Task List:
 //1. Complete articleServices. This module should make an authenticated call and return an array of those articles.
