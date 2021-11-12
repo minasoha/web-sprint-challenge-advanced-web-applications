614f74ae70be189927bd293b5511b17df437acd9
minasoha <minasoha@yahoo.com>
2021-11-12 18:03:48 -0500

    finished logout form

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
-const Logout = () => {        
-    return(<div></div>);
-}
+const Logout = () => {
+  const { push } = useHistory();
+  useEffect(() => {
+    axiosWithAuth()
+      .post("http://localhost:5000/api/logout")
+      .then(() => {
+        localStorage.removeItem("token");
+        push("/login");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  return <div></div>;
+};
 
 export default Logout;
 
